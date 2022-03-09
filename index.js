7a71feb9b6412e19b0ab40baf85cef56f9e4667d
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-03-08 21:46:48 -0500

    commit tree c3c27a8bc93af3b88a4f6c3d35ff041b2ccad0e0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (names, events) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        debugger;
+      }
+      return messages
+    }
+
+function countDown (positiveNumber){
+while (positiveNumber >= 0) {
+  console.log(positiveNumber);
+  positiveNumber--;
+}
+}
